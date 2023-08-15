9ffdc57dbf50e5d68efb0914332de48d9a7d8da5
brittat <thornflower@gmail.com>
2023-08-15 10:42:32 +0200

    Add main.py with simple move parser
    Change-Id: I08e654173acbb5f83db14b71325b41e56e89ddb2

---
+++ chess_sim/main.py
@@ -0,0 +1,13 @@
+import argparse
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+    prog='ChessSimulator',
+    description='Accepts a list of uci compliant chess moves and writes the resulting board state to an svg file',
+    epilog='Text at the bottom of help')
+    parser.add_argument('-np', '--no-print', help='Supress printing the board state')
+#    parser.add_argument('board_svg_filename', required=False, help='File name for resulting state, defaults to chess_sim_{git HEAD short hash}')           
+    parser.add_argument('-ms', '--moves', type=str, nargs='+', help='Chess moves')
+    args = parser.parse_args()
+    print(args.moves)
